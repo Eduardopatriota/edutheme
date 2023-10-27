857df5172fa8cbe9bac3475f510f1bfbdeac6c1a
eduardo_patriota <eduardo.patriota@frncubo.com>
2023-10-27 15:18:06 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="<?php bloginfo('charset')?>">
+   <?php wp_head(); ?>
+</head>
+<body <?php body_class('example'); ?>>
+<?php wp_body_open();?>
+<p>Document</p>
+<?php wp_footer(); ?>
+</body>
+</html>
